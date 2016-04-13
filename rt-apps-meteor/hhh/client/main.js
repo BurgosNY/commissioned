3243cb562f95fadb3c16e24ee263f256eced1aeb
third <asciiascetic@gmail.com>
2016-04-13 16:21:22 -0500

    initial commit on rt-apps-meteor game for tutors4geeks post

---
+++ rt-apps-meteor/hhh/client/main.js
@@ -0,0 +1,92 @@
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+
+import { Players } from '../players.js';
+import { Pellets } from '../pellets.js';
+
+import './main.html';
+
+Template.game.onRendered( function () {
+    let that = this;
+    Tracker.autorun(function () {
+	
+	let gamecanvas = that.find('#gamecanvas'); // get canvas from dom
+	let ctx = gamecanvas.getContext('2d');     // get its 2d drawing context
+	
+	ctx.fillStyle = "rgb(45,45,45)";
+	ctx.fillRect(0,0,500,500);
+	
+	let players = Players.find().fetch();
+	//    console.log(players.count());
+	players.forEach( (p) => {
+	    ctx.fillStyle = p.color;
+	    ctx.beginPath();
+	    ctx.moveTo(p.x, p.y);
+	    ctx.arc( p.x, p.y, 20, 0, 2 * Math.PI);
+	    ctx.fill();
+	});
+
+	let pellets = Pellets.find().fetch();
+	pellets.forEach( (p) => {
+	    ctx.fillStyle = "black";
+	    ctx.fillRect(p.x - 5, p.y - 5, 10, 10);
+	});
+    });
+});
+
+Template.game.onCreated( () => {
+
+    let randomColor = function () {
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+	return 'rgb(' + r + ',' + g + ',' + b + ')';
+    };
+    
+    Session.set('playerid', Players.insert({
+	x: Math.round(Math.random() * 500),
+	y: Math.round(Math.random() * 500),
+	color: randomColor(),
+	points: 0,
+	lastActive: new Date()
+    }));
+
+});
+
+const STEP_SIZE = 10;
+
+Template.game.events({
+    'click' (event) {
+	let playerid = Session.get('playerid');
+	let player = Players.findOne(playerid);
+	let dx = event.offsetX - player.x;
+	let dy = event.offsetY - player.y;
+	let dist = Math.sqrt( dx*dx + dy*dy);
+	let newx =  player.x + dx * (STEP_SIZE / dist);
+	let newy =  player.y + dy * (STEP_SIZE / dist);
+	
+
+	let gotPellet = Pellets.findOne({ $and: [ { x : {$gte: newx - 10}},
+						  { x : {$lte: newx + 10}},
+						  { y : {$gte: newy - 10}},
+						  { y : {$lte: newy + 10}}]});
+	console.log(gotPellet);
+	if (gotPellet) {
+	    Pellets.remove(gotPellet._id);
+	    Players.update( playerid,
+			    {$set: {x : newx,
+				    y : newy,
+				    points: playerid.points + 1,
+				    lastActive: new Date()}});
+	    
+	} else {
+	    Players.update( playerid,
+			    {$set: {x : newx,
+				    y : newy,
+				    lastActive: new Date()}});
+	    
+	}
+				  
+    }
+});
+
---
+++ rt-apps-meteor/hhh/server/main.js
@@ -0,0 +1,35 @@
+import { Meteor } from 'meteor/meteor';
+import { Players } from '../players.js';
+import { Pellets } from '../pellets.js';
+
+Meteor.startup(() => {
+    spawnPellets();
+});
+
+
+
+const PLAYER_TIMEOUT = 20000;
+
+let cleanIdle = function () {
+    let now = new Date();
+    let toRemove = [];
+    Players.find().forEach( (player) => {
+	if (now - player.lastActive > PLAYER_TIMEOUT)
+	    toRemove.push( player._id );
+    });
+
+    toRemove.forEach( (player) => { Players.remove( player ); });
+    
+};
+
+let spawnPellets = function () {
+    if (Math.random() > 0.7 && Pellets.find().count() < 30) {
+	Pellets.insert({x: Math.floor(Math.random() * 500),
+			y: Math.floor(Math.random() * 500)});
+    }
+};
+
+
+
+Meteor.setInterval(cleanIdle, PLAYER_TIMEOUT + 1000);
+Meteor.setInterval(spawnPellets, 1000);
