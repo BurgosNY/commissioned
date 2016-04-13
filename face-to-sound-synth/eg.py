2896cdc134e27186143ececbe2795db1b1f510bf
third <asciiascetic@gmail.com>
2016-04-13 08:19:02 -0500

    better reporting in the test script

--- face-to-sound-synth/eg.py
+++ face-to-sound-synth/eg.py
@@ -5,10 +5,11 @@ import rtmidi
 
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
+proceed_to_send = False
 
 print("number of available ports: " + str(len(available_ports)))
 
-if available_ports:
+if len(available_ports) > 0:
 
     print("which port?")
     print("")
@@ -18,14 +19,19 @@ if available_ports:
     portnum = int(input())
 
     midiout.open_port(portnum)
+    proceed_to_send = True
     print("opening port " + str(portnum))
 else:
-    midiout.open_virtual_port("My virtual output")
+    print("open virtual port? (y/n)")
+    yn = input()
+    if yn == "y":
+        midiout.open_virtual_port("My virtual output")
+        proceed_to_send = True
 
 note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
 note_off = [0x80, 60, 0]
 
-while True:
+while proceed_to_send:
     midiout.send_message(note_on)
     time.sleep(0.5)
     midiout.send_message(note_off)
