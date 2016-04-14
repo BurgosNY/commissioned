6dfda0cc9f6c552229621d3c9e2b28948914d910
third <asciiascetic@gmail.com>
2016-04-13 19:00:38 -0500

    cleaning up code for display

--- rt-apps-meteor/hhh/client/main.js
+++ rt-apps-meteor/hhh/client/main.js
@@ -6,18 +6,16 @@ import { Pellets } from '../pellets.js';
 
 import './main.html';
 
-Template.game.onRendered( function () {
-    let that = this;
+var updateGameBoard = function (gamecanvas) {
     Tracker.autorun(function () {
 	
-	let gamecanvas = that.find('#gamecanvas'); // get canvas from dom
+
 	let ctx = gamecanvas.getContext('2d');     // get its 2d drawing context
 	
 	ctx.fillStyle = "rgb(45,45,45)";
 	ctx.fillRect(0,0,400,400);
 	
 	let players = Players.find().fetch();
-	//    console.log(players.count());
 	players.forEach( (p) => {
 	    ctx.fillStyle = p.color;
 	    ctx.beginPath();
@@ -32,20 +30,27 @@ Template.game.onRendered( function () {
 	    ctx.fillRect(p.x - 5, p.y - 5, 10, 10);
 	});
     });
+}
+
+Template.game.onRendered( function () {
+    let gamecanvas = this.find('#gamecanvas'); // get canvas from dom
+    updateGameBoard(gamecanvas);
 });
+
+let randomColor = function () {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+};
+
+const BOARD_SIZE = 400;
 
 Template.game.onCreated( () => {
 
-    let randomColor = function () {
-	let r = Math.floor(Math.random() * 255);
-	let g = Math.floor(Math.random() * 255);
-	let b = Math.floor(Math.random() * 255);
-	return 'rgb(' + r + ',' + g + ',' + b + ')';
-    };
-    
     Session.set('playerid', Players.insert({
-	x: Math.round(Math.random() * 400),
-	y: Math.round(Math.random() * 400),
+	x: Math.round(Math.random() * BOARD_SIZE),
+	y: Math.round(Math.random() * BOARD_SIZE),
 	color: randomColor(),
 	points: 0,
 	lastActive: new Date()
@@ -55,37 +60,44 @@ Template.game.onCreated( () => {
 
 const STEP_SIZE = 18;
 
+let nearbyPelletQuery = function (newp) {
+    return { $and: [ { x : {$gte: newp.x - 10}},
+		     { x : {$lte: newp.x + 10}},
+		     { y : {$gte: newp.y - 10}},
+		     { y : {$lte: newp.y + 10}}]};
+};
+
+let newPosition = function ( event, player) {
+    let dx = event.offsetX - player.x;
+    let dy = event.offsetY - player.y;
+    let dist = Math.sqrt( dx*dx + dy*dy);
+    let newx =  player.x + dx * (STEP_SIZE / dist);
+    let newy =  player.y + dy * (STEP_SIZE / dist);
+    return {x:newx, y:newy};
+};
+
 Template.game.events({
     'click' (event) {
-	let playerid = Session.get('playerid');
-	let player = Players.findOne(playerid);
-	let dx = event.offsetX - player.x;
-	let dy = event.offsetY - player.y;
-	let dist = Math.sqrt( dx*dx + dy*dy);
-	let newx =  player.x + dx * (STEP_SIZE / dist);
-	let newy =  player.y + dy * (STEP_SIZE / dist);
-	
+	let player = Players.findOne(Session.get('playerid')); 
 
-	let gotPellet = Pellets.findOne({ $and: [ { x : {$gte: newx - 10}},
-						  { x : {$lte: newx + 10}},
-						  { y : {$gte: newy - 10}},
-						  { y : {$lte: newy + 10}}]});
+	let newPos = newPosition(event, player);
+	
+	let gotPellet = Pellets.findOne( nearbyPelletQuery( newPos ) );
+	
 	if (gotPellet) {
 	    Pellets.remove(gotPellet._id);
-	    Players.update( playerid,
-			    {$set: {x : newx,
-				    y : newy,
+	    Players.update( player._id,
+			    {$set: {x : newPos.x,
+				    y : newPos.y,
 				    points: player.points + 1,
 				    lastActive: new Date()}});
 	    
 	} else {
-	    Players.update( playerid,
-			    {$set: {x : newx,
-				    y : newy,
+	    Players.update( player._id,
+			    {$set: {x : newPos.x,
+				    y : newPos.y,
 				    lastActive: new Date()}});
-	    
 	}
-				  
     }
 });
 
