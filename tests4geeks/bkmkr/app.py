bf648fc2e90a5529a6b05aa29b47922f78e477ce
third <asciiascetic@gmail.com>
2016-05-04 07:33:39 -0500

    added error hadnling imports

--- tests4geeks/bkmkr/app.py
+++ tests4geeks/bkmkr/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, json, request, Response
 
+
 # imports for working with mongo from python
 from mongokat import Collection, Document
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 
 # mongokat's Document and Collection classes let you define helper
 # methods for you database collections.  The Document class also lets
 # you define hooks and validators
 
 class ArticleLink(Document):
-    def to_json_data(self):
-        return {'link': self['link'], 'title' : self['title']}
+    def to_json_view(self):
+        return {'_id' : str(self['_id']),
+                'link': self['link'],
+                'title' : self['title']}
 
 
     # TODO: show off 
@@ -22,11 +26,12 @@ class ArticleLinks(Collection):
 
     def search_title(self, qs):
         data = self.find({"$text" : {"$search" : qs}})            
-        return {'links' : [d.to_json_data() for d in data]}
+        return {'links' : [d.to_json_view() for d in data]}
 
     def tagged(self, tags):
         data = self.find({'tags' : {'$in' : tags}});
-        return {'links' : [d.to_json_data() for d in data]}
+        return {'links' : [d.to_json_view() for d in data]}
+
 
 app = Flask(__name__)
 
@@ -36,7 +41,7 @@ LinksClient = ArticleLinks(collection=client.dirt.links)
 @app.route('/')
 def list_article_links():
     data = LinksClient.find()
-    jsondata = {'links' : [d.to_json_data() for d in data]}
+    jsondata = {'links' : [d.to_json_view() for d in data]}
     resp = jsonify(jsondata)
     resp.status_code = 200
     return resp
@@ -54,6 +59,18 @@ def search_title():
     resp.response_code = 200
     return resp 
 
+@app.route('/markread/<linkid>')
+def mark_read(linkid):
+    try:
+        linkdoc = LinksClient.find_one( ObjectId(linkid) )
+        if linkdoc:
+            resp = jsonify(linkdoc.to_json_view())
+            resp.response_code = 200
+            return resp
+        else:
+            return "UH OH"
+    except InvalidId:
+        return "Bad Input"
 
 if __name__ == '__main__':
     app.debug = True
